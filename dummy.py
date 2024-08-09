c0f42cb0f02f36a29ea6c7d0a52202508682ad3a
akshaya666 <60505338+akshaya666@users.noreply.github.com>
2024-08-09 14:47:21 +0530

    Update dummy.py

--- dummy.py
+++ dummy.py
@@ -1,19 +1,26 @@
-.menu-link {
-    text-decoration: none;
-    color: white;
-    position: absolute;
-    left: 20px;
-    top: 50%;
-    transform: translateY(-50%);
-    display: flex;
-    align-items: center;
-    font-size: 16px;
+/* Custom Scrollbar Styling for Chat Window */
+.chat-window {
+    max-height: 500px; /* Adjust as needed */
+    overflow-y: auto;
+    scrollbar-width: thin; /* For Firefox */
+    scrollbar-color: rgba(0, 71, 123, 1) #f3f3f3; /* Thumb and track colors for Firefox */
 }
 
-.menu-link i {
-    margin-right: 5px;
+.chat-window::-webkit-scrollbar {
+    width: 8px; /* Width of the scrollbar */
 }
-if 'Contents' not in response:
-        return []
 
-    file_list = [item['Key'] for item in response['Contents'] if item['Key'] != folder_name]
+.chat-window::-webkit-scrollbar-thumb {
+    background-color: rgba(0, 71, 123, 1); /* Color of the scrollbar thumb */
+    border-radius: 4px; /* Roundness of the scrollbar thumb */
+    border: 2px solid #f3f3f3; /* Padding around the thumb for better aesthetics */
+}
+
+.chat-window::-webkit-scrollbar-thumb:hover {
+    background-color: rgba(0, 71, 123, 0.8); /* Darken the thumb on hover */
+}
+
+.chat-window::-webkit-scrollbar-track {
+    background-color: #f3f3f3; /* Color of the scrollbar track */
+    border-radius: 4px; /* Roundness of the scrollbar track */
+}
